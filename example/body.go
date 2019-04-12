207ebc32a1c9fcb41de6fd7f570df5b1ca48a26b
eihigh <eihigh.contact@gmail.com>
2019-04-12 11:27:32 +0900

    Accept no args for go generate

--- example/body.go
+++ example/body.go
@@ -1,3 +1,4 @@
+//go:generate propgen
 package main
 
 import "time"
--- main.go
+++ main.go
@@ -17,39 +17,64 @@ func main() {
 	app.Name = "propgen"
 	app.Action = func(c *cli.Context) error {
 
-		if c.NArg() != 1 {
-			return xerrors.New("propgen requires one file path")
+		if c.NArg() > 1 {
+			return xerrors.New("too many args")
 		}
 
-		path := c.Args()[0]
-		src, err := ioutil.ReadFile(path)
-		if err != nil {
-			return xerrors.Errorf("cannot read file %v", path)
+		if c.NArg() == 0 {
+			files, err := ioutil.ReadDir(".")
+			if err != nil {
+				return err
+			}
+			for _, file := range files {
+				path := file.Name() // filepath.Join(dir, file.Name())
+				if err := generateFile(path); err != nil {
+					return err
+				}
+			}
+		} else {
+			path := c.Args()[0]
+			if err := generateFile(path); err != nil {
+				return err
+			}
 		}
 
-		result, err := generate(src)
-		if err != nil {
-			return xerrors.Errorf("generate from file %v failed", path)
-		}
-
-		// make output path "src_propgen.go"
-		ext := filepath.Ext(path)
-		head := strings.TrimSuffix(path, ext)
-		outPath := head + "_propgen" + ext
-
-		out, err := os.Create(outPath)
-		if err != nil {
-			return err
-		}
-		defer out.Close()
-
-		out.WriteString(result)
-		fmt.Println("successfully generated", outPath)
 		return nil
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		fmt.Printf("%+v", err)
+		fmt.Printf("propgen: %+v\n", err)
 		os.Exit(1)
 	}
+}
+
+func generateFile(path string) error {
+	src, err := ioutil.ReadFile(path)
+	if err != nil {
+		return xerrors.Errorf("cannot read file %v", path)
+	}
+
+	result, err := generate(src)
+	if err != nil {
+		return xerrors.Errorf("generate from file %v failed", path)
+	}
+
+	if result == "" {
+		return nil
+	}
+
+	// make output path "src_propgen.go"
+	ext := filepath.Ext(path)
+	head := strings.TrimSuffix(path, ext)
+	outPath := head + "_propgen" + ext
+
+	out, err := os.Create(outPath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	out.WriteString(result)
+	fmt.Println("successfully generated", outPath)
+	return nil
 }
